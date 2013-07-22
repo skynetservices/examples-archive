9b44d13b98feb41306f5860db8f78a1a536320ca
Erik St. Martin <alakriti@gmail.com>
2013-07-22 13:35:06 -0400

    start of led stats reporter to blink led when requests are processed

--- pidemo/service/service.go
+++ pidemo/service/service.go
@@ -4,21 +4,81 @@ import (
 	"github.com/skynetservices/skynet2"
 	"github.com/skynetservices/skynet2/log"
 	"github.com/skynetservices/skynet2/service"
+	"github.com/skynetservices/skynet2/stats"
 	"github.com/skynetservices/zkmanager"
 	"os"
 	"strings"
 	"time"
 )
 
+var led *LED = NewLED()
+var registered bool
+
+const (
+	RED = iota
+	GREEN
+	BLUE
+)
+
+type LedReporter struct {
+	blinkChan chan int
+}
+
+func (r *LedReporter) UpdateHostStats(host string, stats stats.Host) {}
+func (r *LedReporter) MethodCalled(method string)                    {}
+func (r *LedReporter) MethodCompleted(method string, duration int64, err error) {
+	if err != nil {
+		r.blinkChan <- RED
+	} else {
+		r.blinkChan <- GREEN
+	}
+}
+
+func (r *LedReporter) watch() {
+	var t *time.Ticker = time.NewTicker(1 * time.Second)
+
+	for {
+		select {
+		case color := <-r.blinkChan:
+			t.Stop()
+
+			switch color {
+			case RED:
+				led.Red(true)
+			case BLUE:
+				led.Blue(true)
+			case GREEN:
+				led.Green(true)
+			}
+
+			t = time.NewTicker(250 * time.Millisecond)
+		case <-t.C:
+			if registered {
+				led.Blue(true)
+			} else {
+				led.Off()
+			}
+		}
+	}
+}
+
+func NewLedReporter() (r *LedReporter) {
+	r = &LedReporter{}
+	go r.watch()
+
+	return
+}
+
 type PiDemoService struct {
-	Led *LED
 }
 
 func (s *PiDemoService) Registered(service *service.Service) {
-	s.Led.Blue(true)
+	registered = true
+	led.Blue(true)
 }
 func (s *PiDemoService) Unregistered(service *service.Service) {
-	s.Led.Blue(false)
+	registered = false
+	led.Blue(false)
 }
 
 func (s *PiDemoService) Started(service *service.Service) {}
@@ -26,9 +86,7 @@ func (s *PiDemoService) Stopped(service *service.Service) {
 }
 
 func NewPiDemoService() *PiDemoService {
-	r := &PiDemoService{
-		Led: NewLED(),
-	}
+	r := &PiDemoService{}
 	return r
 }
 
@@ -39,6 +97,8 @@ func (s *PiDemoService) Upcase(requestInfo *skynet.RequestInfo, in map[string]in
 
 func main() {
 	log.SetLogLevel(log.DEBUG)
+	stats.AddReporter(NewLedReporter())
+
 	skynet.SetServiceManager(zkmanager.NewZookeeperServiceManager(os.Getenv("SKYNET_ZOOKEEPER"), 1*time.Second))
 
 	testService := NewPiDemoService()
